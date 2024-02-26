d82b43406c4c3f846b9344d64277ba3536547f27
Igor Senych <igor.senych@cloudwalk.io>
2024-02-26 17:13:22 +0200

    Reorg

--- src/libraries/Loan.sol
+++ src/libraries/Loan.sol
@@ -8,39 +8,6 @@ import {Interest} from "./Interest.sol";
 /// @author CloudWalk Inc. (See https://cloudwalk.io)
 /// @notice Defines loan related structs and enums
 library Loan {
-    /// @notice A struct that defines the terms of the loan
-    struct Terms {
-        //slot 1
-        /// @notice The address of the token to be used in the loan
-        address token;
-        /// @notice The duration of the loan period specified in seconds
-        uint32 periodInSeconds;
-        /// @notice The total duration of the loan determined by the number of periods
-        uint32 durationInPeriods;
-        /// @notice The rate factor used together with interest rate
-        uint32 interestRateFactor;
-
-        //slot 2
-        /// @notice The address of the recipient of additional payments and fees
-        address addonRecipient;
-        /// @notice The amount of additional payments and fees
-        uint64 addonAmount;
-        /// @notice The primary interest rate to be applied to the loan
-        uint32 interestRatePrimary;
-
-        //slot 3
-        /// @notice The secondary interest rate to be applied to the loan
-        uint32 interestRateSecondary;
-        /// @notice Whether the loan can be repaid automatically
-        bool autoRepayment;
-        /// @notice The formula to be used for interest calculation on the loan
-        Interest.Formula interestFormula;
-
-        //slot 4
-        /// @notice The address of the loan holder
-        address holder;
-    }
-
     /// @notice A struct that defines the state of the loan
     struct State {
         //slot 1
@@ -75,6 +42,39 @@ library Loan {
         /// @notice Whether the loan can be repaid automatically
         bool autoRepayment;
         /// @notice The formula used for interest calculation on the loan
+        Interest.Formula interestFormula;
+
+        //slot 4
+        /// @notice The address of the loan holder
+        address holder;
+    }
+
+    /// @notice A struct that defines the terms of the loan
+    struct Terms {
+        //slot 1
+        /// @notice The address of the token to be used in the loan
+        address token;
+        /// @notice The duration of the loan period specified in seconds
+        uint32 periodInSeconds;
+        /// @notice The total duration of the loan determined by the number of periods
+        uint32 durationInPeriods;
+        /// @notice The rate factor used together with interest rate
+        uint32 interestRateFactor;
+
+        //slot 2
+        /// @notice The address of the recipient of additional payments and fees
+        address addonRecipient;
+        /// @notice The amount of additional payments and fees
+        uint64 addonAmount;
+        /// @notice The primary interest rate to be applied to the loan
+        uint32 interestRatePrimary;
+
+        //slot 3
+        /// @notice The secondary interest rate to be applied to the loan
+        uint32 interestRateSecondary;
+        /// @notice Whether the loan can be repaid automatically
+        bool autoRepayment;
+        /// @notice The formula to be used for interest calculation on the loan
         Interest.Formula interestFormula;
 
         //slot 4
