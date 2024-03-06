b5612e1d6834966dacfe356a4d7fd7603a0131dd
Evgenii Zaitsev <evgenii@cloudwalk.io>
2024-03-06 13:53:52 +0700

    Improve the math library

--- src/libraries/InterestMath.sol
+++ src/libraries/InterestMath.sol
@@ -13,12 +13,12 @@ library InterestMath {
     // -------------------------------------------- //
 
     /*
-    * Minimum value that signed 64.64 bit fixed point number may contain.
-    */
+     * Minimum value that a signed 64.64 bit fixed point number may contain.
+     */
     int128 private constant MIN_64x64_VALUE = -0x80000000000000000000000000000000;
 
     /*
-     * Maximum value that signed 64.64 bit fixed point number may contain.
+     * Maximum value that a signed 64.64 bit fixed point number may contain.
      */
     int128 private constant MAX_64x64_VALUE = 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF;
 
@@ -30,9 +30,9 @@ library InterestMath {
     error InterestFormulaNotImplemented();
 
     /// @notice Thrown when the zero amount was passed as a denominator
-    error ZeroValue();
+    error ZeroDenominator();
 
-    /// @notice Thrown when the calculation results overflows or underflows the bounds of the used type
+    /// @notice Thrown when an overflow or underflow occurs during mathematical operations
     error MathOperationError();
 
     // -------------------------------------------- //
@@ -53,7 +53,12 @@ library InterestMath {
         Interest.Formula interestFormula
     ) internal pure returns (uint256 remainingBalance) {
         if (interestFormula == Interest.Formula.Compound) {
-            return _calculateOutstandingBalanceRounded(originalBalance, numberOfPeriods, interestRate, interestRateFactor);
+            return _calculateOutstandingBalanceRounded(
+                originalBalance,
+                numberOfPeriods,
+                interestRate,
+                interestRateFactor
+            );
         } else {
             revert InterestFormulaNotImplemented();
         }
@@ -94,18 +99,18 @@ library InterestMath {
     }
 
     /**
-    * @dev Calculates the result of dividing unsigned 256-bit integer `numerator` by non-zero unsigned 256-bit integer `denominator`,
-    *      with rounding towards zero.
-    * @dev Reverts if `denominator` is zero or if the operation overflows the 64.64-bit fixed-point range.
-    *
-    * @param numerator The unsigned 256-bit integer numerator.
-    * @param denominator The non-zero unsigned 256-bit integer denominator.
-    *
-    * @return The result as a 64.64-bit fixed-point number.
-    */
+     * @dev Calculates the division of unsigned 256-bit integer `numerator`
+     *      by non-zero unsigned 256-bit integer `denominator` with rounding towards zero.
+     * @dev Reverts if `denominator` is zero or in case of arithmetic overflow.
+     *
+     * @param numerator The unsigned 256-bit integer numerator.
+     * @param denominator The non-zero unsigned 256-bit integer denominator.
+     *
+     * @return The result as a 64.64-bit fixed-point number.
+     */
     function divide(uint256 numerator, uint256 denominator) internal pure returns (int128) {
         if (denominator == 0) {
-            revert ZeroValue();
+            revert ZeroDenominator();
         }
         unchecked {
             uint128 remainder = _divide(numerator, denominator);
@@ -117,18 +122,18 @@ library InterestMath {
     }
 
     /**
-    * @dev Calculates the result of raising a signed 64.64 fixed-point number to the power of an unsigned 256-bit integer.
-    * @dev Reverts in case of arithmetic overflow.
-    *
-    * @param base The signed 64.64 fixed-point base number.
-    * @param exponent The unsigned 256-bit integer exponent.
-    *
-    * @return The result as a signed 64.64 fixed-point number.
-    */
-    function power(int128 base, uint256 exponent) internal pure returns (int128) {
+     * @dev Calculates the raising a signed 64.64 fixed-point number to the power of an unsigned 256-bit integer.
+     * @dev Reverts in case of arithmetic overflow.
+     *
+     * @param base The signed 64.64 fixed-point base number.
+     * @param power The unsigned 256-bit integer power.
+     *
+     * @return The result as a signed 64.64 fixed-point number.
+     */
+    function power(int128 base, uint256 power) internal pure returns (int128) {
         unchecked {
             bool isNegative;
-            if (base < 0 && exponent & 1 == 1) {
+            if (base < 0 && power & 1 == 1) {
                 isNegative = true;
             }
 
@@ -138,62 +143,79 @@ library InterestMath {
 
             if (absoluteBase <= 0x10000000000000000) {
                 absoluteBase <<= 63;
-                while (exponent != 0) {
-                    if (exponent & 0x1 != 0) {
-                        absoluteResult = absoluteResult * absoluteBase >> 127;
+                while (power != 0) {
+                    if (power & 0x1 != 0) {
+                        absoluteResult = (absoluteResult * absoluteBase) >> 127;
                     }
-                    absoluteBase = absoluteBase * absoluteBase >> 127;
+                    absoluteBase = (absoluteBase * absoluteBase) >> 127;
 
-                    if (exponent & 0x2 != 0) {
-                        absoluteResult = absoluteResult * absoluteBase >> 127;
+                    if (power & 0x2 != 0) {
+                        absoluteResult = (absoluteResult * absoluteBase) >> 127;
                     }
-                    absoluteBase = absoluteBase * absoluteBase >> 127;
+                    absoluteBase = (absoluteBase * absoluteBase) >> 127;
 
-                    if (exponent & 0x4 != 0) {
-                        absoluteResult = absoluteResult * absoluteBase >> 127;
+                    if (power & 0x4 != 0) {
+                        absoluteResult = (absoluteResult * absoluteBase) >> 127;
                     }
-                    absoluteBase = absoluteBase * absoluteBase >> 127;
+                    absoluteBase = (absoluteBase * absoluteBase) >> 127;
 
-                    if (exponent & 0x8 != 0) {
-                        absoluteResult = absoluteResult * absoluteBase >> 127;
+                    if (power & 0x8 != 0) {
+                        absoluteResult = (absoluteResult * absoluteBase) >> 127;
                     }
-                    absoluteBase = absoluteBase * absoluteBase >> 127;
+                    absoluteBase = (absoluteBase * absoluteBase) >> 127;
 
-                    exponent >>= 4;
+                    power >>= 4;
                 }
-
                 absoluteResult >>= 64;
             } else {
                 uint256 absoluteBaseShift = 63;
-                if (absoluteBase < 0x1000000000000000000000000) { absoluteBase <<= 32; absoluteBaseShift -= 32; }
-                if (absoluteBase < 0x10000000000000000000000000000) { absoluteBase <<= 16; absoluteBaseShift -= 16; }
-                if (absoluteBase < 0x1000000000000000000000000000000) { absoluteBase <<= 8; absoluteBaseShift -= 8; }
-                if (absoluteBase < 0x10000000000000000000000000000000) { absoluteBase <<= 4; absoluteBaseShift -= 4; }
-                if (absoluteBase < 0x40000000000000000000000000000000) { absoluteBase <<= 2; absoluteBaseShift -= 2; }
-                if (absoluteBase < 0x80000000000000000000000000000000) { absoluteBase <<= 1; absoluteBaseShift -= 1; }
+                if (absoluteBase < 0x1000000000000000000000000) {
+                    absoluteBase <<= 32;
+                    absoluteBaseShift -= 32;
+                }
+                if (absoluteBase < 0x10000000000000000000000000000) {
+                    absoluteBase <<= 16;
+                    absoluteBaseShift -= 16;
+                }
+                if (absoluteBase < 0x1000000000000000000000000000000) {
+                    absoluteBase <<= 8;
+                    absoluteBaseShift -= 8;
+                }
+                if (absoluteBase < 0x10000000000000000000000000000000) {
+                    absoluteBase <<= 4;
+                    absoluteBaseShift -= 4;
+                }
+                if (absoluteBase < 0x40000000000000000000000000000000) {
+                    absoluteBase <<= 2;
+                    absoluteBaseShift -= 2;
+                }
+                if (absoluteBase < 0x80000000000000000000000000000000) {
+                    absoluteBase <<= 1;
+                    absoluteBaseShift -= 1;
+                }
 
                 uint256 resultShift = 0;
-                while (exponent != 0) {
+                while (power != 0) {
                     if (absoluteBaseShift >= 64) {
                         revert MathOperationError();
                     }
 
-                    if (exponent & 0x1 != 0) {
-                        absoluteResult = absoluteResult * absoluteBase >> 127;
+                    if (power & 0x1 != 0) {
+                        absoluteResult = (absoluteResult * absoluteBase) >> 127;
                         resultShift += absoluteBaseShift;
                         if (absoluteResult > 0x100000000000000000000000000000000) {
                             absoluteResult >>= 1;
                             resultShift += 1;
                         }
                     }
-                    absoluteBase = absoluteBase * absoluteBase >> 127;
+                    absoluteBase = (absoluteBase * absoluteBase) >> 127;
                     absoluteBaseShift <<= 1;
                     if (absoluteBase >= 0x100000000000000000000000000000000) {
                         absoluteBase >>= 1;
                         absoluteBaseShift += 1;
                     }
 
-                    exponent >>= 1;
+                    power >>= 1;
                 }
 
                 if (resultShift >= 64) {
@@ -210,18 +232,17 @@ library InterestMath {
     }
 
     /**
-    * @dev Calculates the result of multiplying signed 64.64 fixed-point numbers `multiplicanda` and `multiplier`,
-    *      with rounding down.
-    * @dev Reverts in case of arithmetic overflow.
-    *
-    * @param multiplicanda The first signed 64.64-bit fixed-point number.
-    * @param multiplier The second signed 64.64-bit fixed-point number.
-    *
-    * @return The result as a signed 64.64-bit fixed-point number.
-    */
-    function multiply(int128 multiplicanda, int128 multiplier) internal pure returns (int128) {
+     * @dev Calculates the multiplication of two signed 64.64 fixed-point numbers with rounding down.
+     * @dev Reverts in case of arithmetic overflow.
+     *
+     * @param multiplicand The first signed 64.64-bit fixed-point number.
+     * @param multiplier The second signed 64.64-bit fixed-point number.
+     *
+     * @return The result as a signed 64.64-bit fixed-point number.
+     */
+    function multiply(int128 multiplicand, int128 multiplier) internal pure returns (int128) {
         unchecked {
-            int256 product = int256(multiplicanda) * multiplier >> 64;
+            int256 product = (int256(multiplicand) * multiplier) >> 64;
             if (product < MIN_64x64_VALUE || product > MAX_64x64_VALUE) {
                 revert MathOperationError();
             }
@@ -230,37 +251,53 @@ library InterestMath {
     }
 
     /**
-    * @dev Calculates the result of dividing unsigned 256-bit integer `numerator` by non-zero unsigned 256-bit integer `denominator`,
-    *      with rounding towards zero.
-    * @dev Reverts if `denominator` is zero.
-    * @dev Reverts in case of arithmetic overflow.
-    *
-    * @param numerator The unsigned 256-bit integer numerator.
-    * @param denominator The non-zero unsigned 256-bit integer denominator.
-    *
-    * @return The result as an unsigned 64.64-bit fixed-point number.
-    */
+     * @dev Calculates the division of unsigned 256-bit integer `numerator`
+     *      by non-zero unsigned 256-bit integer `denominator` with rounding towards zero.
+     * @dev Reverts if `denominator` is zero or in case of arithmetic overflow.
+     *
+     * @param numerator The unsigned 256-bit integer numerator.
+     * @param denominator The non-zero unsigned 256-bit integer denominator.
+     *
+     * @return The result as an unsigned 64.64-bit fixed-point number.
+     */
     function _divide(uint256 numerator, uint256 denominator) internal pure returns (uint128) {
         unchecked {
             if (denominator == 0) {
-                revert ZeroValue();
+                revert ZeroDenominator();
             }
 
             uint256 remainder;
 
-            if (numerator <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF)
+            if (numerator <= 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
                 remainder = (numerator << 64) / denominator;
-            else {
+            } else {
                 uint256 msb = 192;
                 uint256 numeratorShifted = numerator >> 192;
-                if (numeratorShifted >= 0x100000000) { numeratorShifted >>= 32; msb += 32; }
-                if (numeratorShifted >= 0x10000) { numeratorShifted >>= 16; msb += 16; }
-                if (numeratorShifted >= 0x100) { numeratorShifted >>= 8; msb += 8; }
-                if (numeratorShifted >= 0x10) { numeratorShifted >>= 4; msb += 4; }
-                if (numeratorShifted >= 0x4) { numeratorShifted >>= 2; msb += 2; }
-                if (numeratorShifted >= 0x2) msb += 1;
+                if (numeratorShifted >= 0x100000000) {
+                    numeratorShifted >>= 32;
+                    msb += 32;
+                }
+                if (numeratorShifted >= 0x10000) {
+                    numeratorShifted >>= 16;
+                    msb += 16;
+                }
+                if (numeratorShifted >= 0x100) {
+                    numeratorShifted >>= 8;
+                    msb += 8;
+                }
+                if (numeratorShifted >= 0x10) {
+                    numeratorShifted >>= 4;
+                    msb += 4;
+                }
+                if (numeratorShifted >= 0x4) {
+                    numeratorShifted >>= 2;
+                    msb += 2;
+                }
+                if (numeratorShifted >= 0x2) {
+                    msb += 1;
+                }
 
-                remainder = (numerator << 255 - msb) / ((denominator - 1 >> msb - 191) + 1);
+                remainder = (numerator << (255 - msb)) / (((denominator - 1) >> (msb - 191)) + 1);
                 if (remainder > 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
                     revert MathOperationError();
                 }
@@ -271,17 +308,21 @@ library InterestMath {
                 uint256 numeratorHigh = numerator >> 192;
                 uint256 numeratorLow = numerator << 64;
 
-                if (numeratorLow < low) numeratorHigh -= 1;
+                if (numeratorLow < low) {
+                    numeratorHigh -= 1;
+                }
                 numeratorLow -= low;
                 low = high << 128;
-                if (numeratorLow < low) numeratorHigh -= 1;
+                if (numeratorLow < low) {
+                    numeratorHigh -= 1;
+                }
                 numeratorLow -= low;
 
                 remainder += numeratorHigh == high >> 128 ? numeratorLow / denominator : 1;
             }
 
             if (remainder > 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF) {
-                revert ZeroValue();
+                revert MathOperationError();
             }
             return uint128(remainder);
         }
