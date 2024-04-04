95a0dfc4dd18d7a40dc3887c9f1a7eae2f2de981
Igor Senych <igor.senych@cloudwalk.io>
2024-04-04 17:21:23 +0300

    feat: final review

--- src/common/interfaces/core/ICreditLine.sol
+++ src/common/interfaces/core/ICreditLine.sol
@@ -6,13 +6,13 @@ import { Loan } from "src/common/libraries/Loan.sol";
 
 /// @title ICreditLine interface
 /// @author CloudWalk Inc. (See https://cloudwalk.io)
-/// @notice Defines the credit line contract functions and events.
+/// @dev Defines the credit line contract functions and events.
 interface ICreditLine {
     // -------------------------------------------- //
     //  Functions                                   //
     // -------------------------------------------- //
 
-    /// @notice A hook that is triggered by the market before a loan is taken.
+    /// @dev A hook that is triggered by the associated market before a loan is taken.
     /// @param borrower The address of the borrower.
     /// @param borrowAmount The desired amount of tokens to borrow.
     /// @param durationInPeriods The desired duration of the loan in periods.
@@ -25,7 +25,7 @@ interface ICreditLine {
         uint256 loanId
     ) external returns (Loan.Terms memory terms);
 
-    /// @notice Retrieves the loan terms for the provided borrower and the amount.
+    /// @dev Retrieves the loan terms for the provided borrower, amount, and loan duration.
     /// @param borrower The address of the borrower.
     /// @param borrowAmount The desired amount of tokens to borrow.
     /// @param durationInPeriods The desired duration of the loan in periods.
@@ -36,16 +36,16 @@ interface ICreditLine {
         uint256 durationInPeriods
     ) external view returns (Loan.Terms memory terms);
 
-    /// @notice Returns the address of the associated lending market.
+    /// @dev Returns the address of the associated lending market.
     function market() external view returns (address);
 
-    /// @notice Returns the address of the credit line lender.
+    /// @dev Returns the address of the credit line lender.
     function lender() external view returns (address);
 
-    /// @notice Returns the address of the credit line token.
+    /// @dev Returns the address of the credit line token.
     function token() external view returns (address);
 
-    /// @notice Returns the kind of the credit line.
+    /// @dev Returns the kind of the credit line.
     function kind() external view returns (uint16);
 
     function migrateLoan(uint256 loanId) external;
