5f57f9e7d0b33cd342a526af95651664df18bda1
Igor Senych <igor.senych@cloudwalk.io>
2024-04-04 17:42:22 +0300

    feat: final review

--- src/common/interfaces/core/ILendingRegistry.sol
+++ src/common/interfaces/core/ILendingRegistry.sol
@@ -4,27 +4,27 @@ pragma solidity 0.8.24;
 
 /// @title ILendingRegistry interface
 /// @author CloudWalk Inc. (See https://cloudwalk.io)
-/// @notice Defines the lending registry contract functions and events.
+/// @dev Defines the lending registry contract functions and events.
 interface ILendingRegistry {
     // -------------------------------------------- //
     //  Functions                                   //
     // -------------------------------------------- //
 
-    /// @notice Creates a new credit line.
+    /// @dev Creates a new credit line.
     /// @param kind The kind of the credit line to create.
     /// @param token The address of the credit line token.
     function createCreditLine(uint16 kind, address token) external;
 
-    /// @notice Creates a new liquidity pool.
+    /// @dev Creates a new liquidity pool.
     /// @param kind The kind of the liquidity pool to create.
     function createLiquidityPool(uint16 kind) external;
 
-    /// @notice Returns the address of the credit line factory.
+    /// @dev Returns the address of the credit line factory.
     function creditLineFactory() external view returns (address);
 
-    /// @notice Returns the address of the liquidity pool factory.
+    /// @dev Returns the address of the liquidity pool factory.
     function liquidityPoolFactory() external view returns (address);
 
-    /// @notice Returns the address of the associated lending market.
+    /// @dev Returns the address of the associated lending market.
     function market() external view returns (address);
 }
