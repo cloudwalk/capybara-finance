a4c424112ea1b99fbea798612aa5dce41c112f43
Igor Senych <igor.senych@cloudwalk.io>
2024-04-04 18:40:40 +0300

    feat: final review

--- src/LendingRegistryUUPS.sol
+++ src/LendingRegistryUUPS.sol
@@ -7,7 +7,7 @@ import { LendingRegistry } from "./LendingRegistry.sol";
 
 /// @title LendingRegistryUUPS contract
 /// @author CloudWalk Inc. (See https://cloudwalk.io)
-/// @notice Upgradeable version of the lending market contract.
+/// @dev Upgradeable version of the lending market contract.
 contract LendingRegistryUUPS is LendingRegistry, UUPSUpgradeable {
     /// @dev Constructor that prohibits the initialization of the implementation of the upgradable contract.
     /// @custom:oz-upgrades-unsafe-allow constructor
@@ -16,5 +16,5 @@ contract LendingRegistryUUPS is LendingRegistry, UUPSUpgradeable {
     }
 
     /// @inheritdoc UUPSUpgradeable
-    function _authorizeUpgrade(address newImplementation) internal override onlyOwner { }
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
